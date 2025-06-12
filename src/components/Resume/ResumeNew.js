0bc5df68788d8225b3350c0d03c1bd1bbe608c6f
Vedant Mehta <veds.mehta@gmail.com>
2025-06-11 20:26:25 -0400

    updated information

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Vedant Mehta 2_24_25.pdf";
+import pdf from "../../Assets/TESTRESUME.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
